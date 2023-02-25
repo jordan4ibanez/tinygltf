a41a7e19baa6388472ba5c7a8e8fcb241c048a52
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-02-24 19:15:20 -0500

    Put in this noice description

--- source/tiny_gltf.d
+++ source/tiny_gltf.d
@@ -744,7 +744,15 @@ class Mesh {
 
     this() {}
 }
-
+/**
+    A node in the node hierarchy.
+    When the node contains skin, all mesh.primitives MUST contain JOINTS_0 and WEIGHTS_0 attributes.
+    A node MAY have either a matrix or any combination of translation/rotation/scale (TRS) properties.
+    TRS properties are converted to matrices and postmultiplied in the T * R * S order to compose the transformation matrix.
+    First the scale is applied to the vertices, then the rotation, and then the translation.
+    If none are provided, the transform is the identity.
+    When a node is targeted for animation (referenced by an animation.channel.target), matrix MUST NOT be present.
+*/
 class Node {
 
 public:
